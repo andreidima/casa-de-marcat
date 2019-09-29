90c228d3488fb08f351850a94cecdcaa9a8a8681
‘Andrei <andrei.dima@usm.ro>
2019-09-29 22:30:27 +0300

    'diverse'

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,7 @@ Auth::routes([
 // Route::get('/home', 'HomeController@index')->name('home');
 
 
-// Route::group(['middleware' => 'auth'], function () {
+Route::group(['middleware' => 'auth'], function () {
 
     Route::redirect('/', '/produse');
 
@@ -40,4 +40,4 @@ Auth::routes([
     return "Cleared!";
     });
 
-// });
+});
