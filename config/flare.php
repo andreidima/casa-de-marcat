d1f483111876695e2eba65fbb7504f9120888897
‘Andrei <andrei.dima@usm.ro>
2019-12-30 09:32:00 +0200

    'a'

--- config/flare.php
+++ config/flare.php
@@ -26,23 +26,13 @@ return [
 
     'reporting' => [
         'anonymize_ips' => true,
-        'collect_git_information' => true,
+        'collect_git_information' => false,
         'report_queries' => true,
         'maximum_number_of_collected_queries' => 200,
         'report_query_bindings' => true,
         'report_view_data' => true,
         'grouping_type' => null,
     ],
-
-    // 'collect_git_information' => false,
-    // 'reporting' => [
-    //     'anonymize_ips' => true,
-    //     'collect_git_information' => false,
-    //     'report_queries' => true,
-    //     'maximum_number_of_collected_queries' => 200,
-    //     'report_query_bindings' => true,
-    //     'report_view_data' => true,
-    // ],
 
     /*
     |--------------------------------------------------------------------------
