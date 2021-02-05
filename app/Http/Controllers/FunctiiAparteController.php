9967fcd444757ec0c1dee587aec7d49369f330bc
‘Andrei <andrei.dima@usm.ro>
2021-02-05 17:31:51 +0200

    a

--- app/Http/Controllers/FunctiiAparteController.php
+++ app/Http/Controllers/FunctiiAparteController.php
@@ -22,14 +22,13 @@ table, th, td {
   border-collapse: collapse;
 }
 </style>';
-        echo 'Total produse in subcategoria Folii protectie = ' . $produse->count();
-        echo '<br><br>';
+        // echo 'Total produse in subcategoria Folii protectie = ' . $produse->count();
+        // echo '<br><br>';
 
         echo 'Produsele modificate sunt:';
         echo '<br><br>';
         echo '<table style="border: 1px solid black;">
             <tr>
-                <td>ID</td>
                 <td>Nume</td>
                 <td>Barcode</td>
                 <td>Pret vechi</td>            
@@ -37,22 +36,104 @@ table, th, td {
             </tr>';
         foreach ($produse as $produs){
             switch ($produs->pret) {
-                case '9':
-                    $produs->pret = 10; 
+                case '15':
+                    $produs->pret = 5; 
                     $produs->save();
                     echo 
                         '<tr>' .
-                            '<td>' . $produs->id . '</td>' .
                             '<td>' . $produs->nume . '</td>' .
                             '<td>' . $produs->cod_de_bare . '</td>' .
-                            '<td style="text-align:right">' . '9' . '</td>' .
+                            '<td style="text-align:right">' . '15' . '</td>' .
                             '<td style="text-align:right">' . $produs->pret . '</td>' .
                         '</tr>';
                     break;
-                case '24':
-                    $produs->pret = 25;
+                case '55':
+                    $produs->pret = 40;
                     $produs->save();
-                    echo '<tr><td>', 'Nume';
+                    echo
+                        '<tr>' .
+                            '<td>' . $produs->nume . '</td>' .
+                            '<td>' . $produs->cod_de_bare . '</td>' .
+                            '<td style="text-align:right">' . '55' . '</td>' .
+                            '<td style="text-align:right">' . $produs->pret . '</td>' .
+                            '</tr>';
+                    break;
+                case '70':
+                    $produs->pret = 55;
+                    $produs->save();
+                    echo
+                        '<tr>' .
+                            '<td>' . $produs->nume . '</td>' .
+                            '<td>' . $produs->cod_de_bare . '</td>' .
+                            '<td style="text-align:right">' . '70' . '</td>' .
+                            '<td style="text-align:right">' . $produs->pret . '</td>' .
+                            '</tr>';
+                    break;
+                case '65':
+                    $produs->pret = 50;
+                    $produs->save();
+                    echo
+                        '<tr>' .
+                            '<td>' . $produs->nume . '</td>' .
+                            '<td>' . $produs->cod_de_bare . '</td>' .
+                            '<td style="text-align:right">' . '65' . '</td>' .
+                            '<td style="text-align:right">' . $produs->pret . '</td>' .
+                            '</tr>';
+                    break;
+                case '20':
+                    $produs->pret = 10;
+                    $produs->save();
+                    echo
+                        '<tr>' .
+                            '<td>' . $produs->nume . '</td>' .
+                            '<td>' . $produs->cod_de_bare . '</td>' .
+                            '<td style="text-align:right">' . '20' . '</td>' .
+                            '<td style="text-align:right">' . $produs->pret . '</td>' .
+                            '</tr>';
+                    break;
+                case '90':
+                    $produs->pret = 70;
+                    $produs->save();
+                    echo
+                        '<tr>' .
+                            '<td>' . $produs->nume . '</td>' .
+                            '<td>' . $produs->cod_de_bare . '</td>' .
+                            '<td style="text-align:right">' . '90' . '</td>' .
+                            '<td style="text-align:right">' . $produs->pret . '</td>' .
+                            '</tr>';
+                    break;
+                case '25':
+                    $produs->pret = 15;
+                    $produs->save();
+                    echo
+                        '<tr>' .
+                            '<td>' . $produs->nume . '</td>' .
+                            '<td>' . $produs->cod_de_bare . '</td>' .
+                            '<td style="text-align:right">' . '25' . '</td>' .
+                            '<td style="text-align:right">' . $produs->pret . '</td>' .
+                            '</tr>';
+                    break;
+                case '60':
+                    $produs->pret = 45;
+                    $produs->save();
+                    echo
+                        '<tr>' .
+                            '<td>' . $produs->nume . '</td>' .
+                            '<td>' . $produs->cod_de_bare . '</td>' .
+                            '<td style="text-align:right">' . '60' . '</td>' .
+                            '<td style="text-align:right">' . $produs->pret . '</td>' .
+                            '</tr>';
+                    break;
+                case '45':
+                    $produs->pret = 30;
+                    $produs->save();
+                    echo
+                        '<tr>' .
+                            '<td>' . $produs->nume . '</td>' .
+                            '<td>' . $produs->cod_de_bare . '</td>' .
+                            '<td style="text-align:right">' . '45' . '</td>' .
+                            '<td style="text-align:right">' . $produs->pret . '</td>' .
+                            '</tr>';
                     break;
                 
                 default:
